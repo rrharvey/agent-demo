9f23f645e875c7b39440054280b44ba5e95946ed
Ryan Harvey <rharvey@intertech.com>
2025-04-09 16:57:26 +0000

    Scaffolding for a React app.

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
   {
@@ -12,6 +15,25 @@ export default tseslint.config(
   js.configs.recommended,
   // TypeScript configurations
   ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
   // Prettier configuration (disables rules that conflict with Prettier)
   eslintConfigPrettier
 )
