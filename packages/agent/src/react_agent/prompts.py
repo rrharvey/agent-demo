352fc8bf3688a372d6aeeaf3ab03e83a5f739337
Ryan Harvey <rharvey@intertech.com>
2025-04-15 13:14:56 +0000

    Start over with the agent code

--- packages/agent/src/react_agent/prompts.py
+++
@@ -1,8 +0,0 @@
-"""Default prompts used by the agent."""
-
-SYSTEM_PROMPT = """You are a terse assistant tasked with helping
-Intertech employees track time spent on projects. You may not help
-the user with anything else. Client IDs and Project IDs must not be
-shown to the user.
-
-System time: {system_time}"""
